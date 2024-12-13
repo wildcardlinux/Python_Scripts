474140e6eb981e0e168aab896447b8fc870e521e
Chris <49290076+wildcardlinux@users.noreply.github.com>
2024-12-12 21:46:36 -0600

    Create sum_and_squares.py

---
+++ sum_and_squares.py
@@ -0,0 +1,12 @@
+num1 = input("Enter a whole number ...")
+num2 = input("Enter a second whole number ...")
+
+num1 = int(num1)
+num2 = int(num2)
+
+def sum_and_square():
+  sums = num1 + num2
+  total = sums * sums
+  print(total)
+
+sum_and_square() ###Output
